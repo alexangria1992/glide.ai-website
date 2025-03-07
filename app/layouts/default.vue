256a249c075e32b5530fa75457d1cd7f17653c78
Alexandre Angrignon <alex040892@gmail.com>
2025-03-07 17:03:57 +1300

    Adding settings content on prismic

---
+++ app/layouts/default.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const prismic = usePrismic()
+
+const {data: settings} = await useAsyncData(() => prismic.client.getSingle('settings'))
+
+useSeoMeta({
+  title: settings.value?.data.site_title,
+  ogYTitle: settings.value?.data.site_title,
+  description: settings.value?.data.meta_description,
+  ogDescription: settings.value?.data.meta_description,
+  ogImage: computed(() => prismic.asImageSrc(settings.value?.data.meta_image))
+})
+</script>
+
+<template>
+  <div>
+    {{ settings }}
+    <slot />
+  </div>
+</template>
