10d8b6a285b01ea09f304a15a250fd8f8b5f0cb5
Alexandre Angrignon <alex040892@gmail.com>
2025-03-21 10:11:06 +1300

    add showcase slice

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss';
-
+import tailwindTypography from '@tailwindcss/typography';
 export default <Partial<Config>>{
   content: ['./app/**/*.{js,ts,vue}'],
+  plugins: [tailwindTypography],
 };
